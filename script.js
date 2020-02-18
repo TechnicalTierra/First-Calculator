2d83a84052d8811c464c49612a5f3ace5ccfba77
Tierra <tierrarucker@ymail.com>
2020-02-18 04:31:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Calculator 
+
+// % (percentage)
+function getPercentage(x) {
+  return x * 0.10; // 
+  }
+
+// √ (square root)
+function takeSquareRoot(x)
+
+// x² (squared)
+
+// 
+
+// 
+
+// 
+

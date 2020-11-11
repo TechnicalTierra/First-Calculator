825cc6003c471724d28b511fb0bf0ea384c774a3
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2020-11-10 19:18:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-// Calculator 
+// alert("Calculator js file is attached.");
 
-// % (percentage)
-function getPercentage(x) {
-  return x * 0.10; // 
-  }
+var calculationOutput = document.getElementById("calculation-output").value;
 
-// √ (square root)
-function takeSquareRoot(x)
+var calculationDisplayOutput = document.getElementById("display-output");
+// calculationDisplayOutput = document.getElementById("display-output").innerHTML = "js script test";
 
-// x² (squared)
+//let runningTotal = 0;
 
-// 
+let buffer = "0"; // what's displayed at any given time 
+// keep 'source of truth' (query) OUT of DOM 
 
-// 
 
-// 
+// stsateful interface = keeping track of previous actions 
+//let previousOperator;
 
+//const screen = document.querySelector('.screen');
+
+// js = breaking things into smaller and smaller problems until they're easiter to solve
+
+
+// 1st step - what happens when a button is clicked 
+//function buttonClick(value) {}
+
+
+
+// calculator number buttons
+var oneBtn = document.getElementById("calc-one");
+var twoBtn = document.getElementById("calc-two");
+var threeBtn = document.getElementById("calc-three");
+var fourBtn = document.getElementById("calc-four");
+var fiveBtn = document.getElementById("calc-five");
+var sixBtn = document.getElementById("calc-six");
+var sevenBtn = document.getElementById("calc-seven");
+var eightBtn = document.getElementById("calc-eight");
+var nineBtn = document.getElementById("calc-nine");
+var zeroBtn = document.getElementById("calc-zero");
+// calculation buttons
+var decimalBtn = document.getElementById("calc-decimal");
+var clearBtn;
+var backspaceBtn = document.getElementById("");
